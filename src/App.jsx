2dc48fcab706d40715f1796dcf65e6878c5b9a35
Vishant Kalwani <vishanto95@gmail.com>
2023-09-29 02:17:52 +0530

    Setup simple navbar with 3 icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import { FiBell as Bell } from 'react-icons/fi'
-import { FiSearch as Search } from 'react-icons/fi'
-import { FiMenu as Menu } from 'react-icons/fi'
-import { IconContext } from 'react-icons'
-
+import { Navbar } from './components/Navbar'
 
 function App() {
   return (
-    <IconContext.Provider value={{ size: '1.2rem' }}>
-      <header 
-        className="text-off-white h-12 bg-rich-black flex justify-around items-center"
-      >
-        <button>
-          <Menu />
-        </button>
-        <nav className='flex gap-3'>
-          <button className=''><Bell /></button>
-          <button><Search /></button>
-        </nav>
-      </header>
-    </IconContext.Provider>
+    <Navbar />
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { FiBell as Bell } from 'react-icons/fi'
+import { FiSearch as Search } from 'react-icons/fi'
+import { FiMenu as Menu } from 'react-icons/fi'
+import { IconContext } from 'react-icons'
+
+import { IconButton } from '@material-tailwind/react'
+
+function Navbar() {
+    return (
+        <IconContext.Provider value={{ size: '1.3rem' }}>
+            <header 
+                className="text-off-white h-14 bg-rich-black 
+                flex justify-around items-center"
+            >
+                <IconButton 
+                size='sm'
+                ripple={true} 
+                variant='text' 
+                className='rounded-full hover:bg-gray-800 text-off-white'
+                >
+                <Menu />
+                </IconButton>
+                <nav className='flex gap-3'>
+                <IconButton 
+                    size='sm'
+                    ripple={true} 
+                    variant='text' 
+                    className='rounded-full hover:bg-gray-800 text-off-white'
+                >
+                    <Bell />
+                </IconButton>
+                <IconButton 
+                    size='sm'
+                    ripple={true} 
+                    variant='text' 
+                    className='rounded-full hover:bg-gray-800 text-off-white'
+                >
+                    <Search />
+                </IconButton>
+                </nav>
+            </header>
+        </IconContext.Provider>
+    )
+}
+
+export default Navbar
