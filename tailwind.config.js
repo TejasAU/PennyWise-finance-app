ae2da1edb6a043d600005c77eea04eef7d5a2b52
Vishant Kalwani <vishanto95@gmail.com>
2023-09-30 18:24:28 +0530

    Added Flowbite + ApexCharts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,82 +3,86 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default withMT({
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     // Path to the Tremor module
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+    // Path to Flowbite
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
-  theme: {transparent: "transparent",
-  current: "currentColor",
-  extend: {
-    colors: {
-      'rich-black' : '#0C1325',
-      'navy-blue' : '#0F172A',
-      'dark-green' : '#06976A',
-      'off-white' : '#F0F5F8',
-      'emerald' : '#34D399',
-      // light mode
-      tremor: {
-        brand: {
-          faint: "#eff6ff", // blue-50
-          muted: "#bfdbfe", // blue-200
-          subtle: "#60a5fa", // blue-400
-          DEFAULT: "#3b82f6", // blue-500
-          emphasis: "#1d4ed8", // blue-700
-          inverted: "#ffffff", // white
+  theme: {
+    transparent: "transparent",
+    current: "currentColor",
+    extend: {
+      colors: {
+        // light mode
+        tremor: {
+          brand: {
+            faint: "#eff6ff", // blue-50
+            muted: "#bfdbfe", // blue-200
+            subtle: "#60a5fa", // blue-400
+            DEFAULT: "#3b82f6", // blue-500
+            emphasis: "#1d4ed8", // blue-700
+            inverted: "#ffffff", // white
+          },
+          background: {
+            muted: "#f9fafb", // gray-50
+            subtle: "#f3f4f6", // gray-100
+            DEFAULT: "#ffffff", // white
+            emphasis: "#374151", // gray-700
+          },
+          border: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          ring: {
+            DEFAULT: "#e5e7eb", // gray-200
+          },
+          content: {
+            subtle: "#9ca3af", // gray-400
+            DEFAULT: "#6b7280", // gray-500
+            emphasis: "#F0F5F8", // gray-700 <-- changed to off-white
+            strong: "#111827", // gray-900
+            inverted: "#ffffff", // white
+          },
         },
-        background: {
-          muted: "#f9fafb", // gray-50
-          subtle: "#f3f4f6", // gray-100
-          DEFAULT: "#ffffff", // white
-          emphasis: "#374151", // gray-700
+        // dark mode
+        "dark-tremor": {
+          brand: {
+            faint: "#0B1229", // custom
+            muted: "#172554", // blue-950
+            subtle: "#1e40af", // blue-800
+            DEFAULT: "#3b82f6", // blue-500
+            emphasis: "#60a5fa", // blue-400
+            inverted: "#030712", // gray-950
+          },
+          background: {
+            muted: "#131A2B", // custom
+            subtle: "#1f2937", // gray-800
+            DEFAULT: "#111827", // gray-900
+            emphasis: "#d1d5db", // gray-300
+          },
+          border: {
+            DEFAULT: "#1f2937", // gray-800
+          },
+          ring: {
+            DEFAULT: "#1f2937", // gray-800
+          },
+          content: {
+            subtle: "#4b5563", // gray-600
+            DEFAULT: "#F0F5F8", // gray-500
+            emphasis: "#e5e7eb", // gray-200
+            strong: "#f9fafb", // gray-50
+            inverted: "#000000", // black
+          },
         },
-        border: {
-          DEFAULT: "#e5e7eb", // gray-200
-        },
-        ring: {
-          DEFAULT: "#e5e7eb", // gray-200
-        },
-        content: {
-          subtle: "#9ca3af", // gray-400
-          DEFAULT: "#6b7280", // gray-500
-          emphasis: "#374151", // gray-700
-          strong: "#111827", // gray-900
-          inverted: "#ffffff", // white
-        },
+        'rich-black' : '#0C1325',
+        'navy-blue' : '#0F172A',
+        'dark-green' : '#06976A',
+        'off-white' : '#F0F5F8',
+        'emerald' : '#34D399',
       },
-      // dark mode
-      "dark-tremor": {
-        brand: {
-          faint: "#0B1229", // custom
-          muted: "#172554", // blue-950
-          subtle: "#1e40af", // blue-800
-          DEFAULT: "#3b82f6", // blue-500
-          emphasis: "#60a5fa", // blue-400
-          inverted: "#030712", // gray-950
-        },
-        background: {
-          muted: "#131A2B", // custom
-          subtle: "#1f2937", // gray-800
-          DEFAULT: "#111827", // gray-900
-          emphasis: "#d1d5db", // gray-300
-        },
-        border: {
-          DEFAULT: "#1f2937", // gray-800
-        },
-        ring: {
-          DEFAULT: "#1f2937", // gray-800
-        },
-        content: {
-          subtle: "#4b5563", // gray-600
-          DEFAULT: "#6b7280", // gray-500
-          emphasis: "#e5e7eb", // gray-200
-          strong: "#f9fafb", // gray-50
-          inverted: "#000000", // black
-        },
-      },
-    },
     boxShadow: {
       // light
       "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
@@ -131,5 +135,10 @@ safelist: [
       /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
   },
 ],
-plugins: [require("@headlessui/tailwindcss")],
+plugins: [
+  require("@headlessui/tailwindcss"),
+  require('flowbite/plugin')({
+    charts: true
+  })
+],
 });
