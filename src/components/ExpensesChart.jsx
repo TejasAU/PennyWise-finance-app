7d5244325c0743b408ec536a4b8bd85ec670b2e6
Vishant Kalwani <vishanto95@gmail.com>
2023-09-30 22:47:46 +0530

    Refactored Pie chart into separate component

---
+++ src/components/ExpensesChart.jsx
@@ -0,0 +1,45 @@
+import { DonutChart, Legend } from "@tremor/react";
+
+const budgetData = [
+    {
+        category: 'Food',
+        value: 300,
+    },
+    {
+        category: 'Transport',
+        value: 220,
+    },
+    {
+        category: 'Entertainment',
+        value: 150,
+    },
+];
+
+const valueFormatter = (number) => `Rs. ${number}`
+
+/* Use the following colors: pink, purple, indigo, blue, cyan, teal, yellow, lime, green
+    amber, orange, red and gray
+*/
+
+export function ExpensesChart() {
+    return(
+        <div className="flex flex-col items-center justify-center">
+            <DonutChart
+                    className="mt-6"
+                    data={budgetData}
+                    category="value"
+                    index="category"
+                    // TODO generate random color array
+                    colors={["purple","red","cyan"]}
+                    valueFormatter={valueFormatter}
+            />
+            <Legend
+                className="mt-3 m"
+                // TODO get dynamically from data
+                categories={["Food","Transport","Entertainment"]}
+                // TODO generate random color array
+                colors={["purple","red","cyan"]}
+            />
+        </div>
+    )
+}
