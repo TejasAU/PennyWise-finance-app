26e05097f73e4223b662bfb9aa62bcc5b6a0d6b7
Vishant Kalwani <vishanto95@gmail.com>
2023-10-01 13:33:43 +0530

    Linking of login and home done

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import { createContext, useState } from 'react'
 import { Header } from './components/navbar/Header'
 import { Sidebar } from './components/sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
+import { Login } from './components/SignUp/Login'
 
 export const AppContext = createContext()
 
 function App() {
   const [isMounted, setIsMounted] = useState(true)
+  const [isAuth, setIsAuth] = useState(false)
 
   return (
-      <AppContext.Provider value = { {isMounted, setIsMounted}}>  
-        <div className='flex h-full w-full'>
-          {isMounted && <Sidebar />}
-          <div className='flex flex-col h-full w-full'>
-            <Header />
-            <Outlet />
-          </div>
-        </div>
+      <AppContext.Provider value = { {isMounted, setIsMounted, isAuth, setIsAuth}}>  
+        { isAuth ? ( 
+          <div className='flex h-full w-full'>
+            {isMounted && <Sidebar />}
+            <div className='flex flex-col h-full w-full'>
+              <Header />
+              <Outlet />
+            </div>
+          </div> )
+          : (
+            <Login />
+          )
+        }
       </AppContext.Provider>
   )
 }
--- src/components/SignUp/Login.jsx
+++ src/components/SignUp/Login.jsx
@@ -1,7 +1,20 @@
+import { useContext } from "react"
+import { AppContext } from "../../App"
+import { Button } from "@material-tailwind/react"
+
 export function Login() {
+    const { setIsAuth } = useContext(AppContext)
+
     return(
         <main className="bg-rich-black text-off-white h-full">
             This is a login page
+            <Button 
+                variant="outlined"
+                onClick={ () => setIsAuth(true)}
+                className="text-off-white border border-emerald"
+            >
+                Go to Home Page
+            </Button>
         </main>
     )
 }
