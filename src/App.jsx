5e43c9b4c959f703ddf5051cbafa2efde31edeca
Vishant Kalwani <vishanto95@gmail.com>
2023-10-01 14:02:55 +0530

    Added welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Header } from './components/navbar/Header'
 import { Sidebar } from './components/sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
 import { Login } from './components/SignUp/Login'
+import { useOutlet } from 'react-router-dom'
+import { Welcome } from './components/Welcome'
 
 export const AppContext = createContext()
 
 function App() {
   const [isMounted, setIsMounted] = useState(true)
   const [isAuth, setIsAuth] = useState(false)
+  const outlet = useOutlet()
 
   return (
       <AppContext.Provider value = { {isMounted, setIsMounted, isAuth, setIsAuth}}>  
@@ -17,7 +20,7 @@ function App() {
             {isMounted && <Sidebar />}
             <div className='flex flex-col h-full w-full'>
               <Header />
-              <Outlet />
+              {outlet || <Welcome />}
             </div>
           </div> )
           : (
