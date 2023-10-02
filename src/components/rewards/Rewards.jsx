160dadb358d23fa0a72452da5629c7564a875324
TejasAU <91965903+TejasAU@users.noreply.github.com>
2023-10-02 16:10:03 +0530

    added style

--- src/components/rewards/Rewards.jsx
+++ src/components/rewards/Rewards.jsx
@@ -9,66 +9,59 @@ import {
     Tab,
     TabPanel,
   } from "@material-tailwind/react";
-
+  import React from "react";
+  import { GiBreakingChain as Streak } from "react-icons/gi"
+  import { LiaCoinsSolid as Points } from "react-icons/lia"
 export function Rewards() {
     const data = [
         {
-          label: "HTML",
+          label: "Point Rewards",
           value: "html",
-          desc: `It really matters and then like it really doesn't matter.
-          What matters is the people who are sparked by it. And the people
-          who are like offended by it, it doesn't matter.`,
+          desc: `Complete challenges to earn points.`,
+          icon: <Points/> ,
         },
         {
-          label: "React",
+          label: "Streak Rewards",
           value: "react",
-          desc: `Because it's about motivating the doers. Because I'm here
-          to follow my dreams and inspire other people to follow their dreams, too.`,
+          desc: `Complete streaks to get rewards`,
+          icon: <Streak /> ,
         },
      
-        {
-          label: "Vue",
-          value: "vue",
-          desc: `We're not always in the position that we want to be at.
-          We're constantly growing. We're constantly making mistakes. We're
-          constantly trying to express ourselves and actualize our dreams.`,
-        },
-     
-        {
-          label: "Angular",
-          value: "angular",
-          desc: `Because it's about motivating the doers. Because I'm here
-          to follow my dreams and inspire other people to follow their dreams, too.`,
-        },
-     
-        {
-          label: "Svelte",
-          value: "svelte",
-          desc: `We're not always in the position that we want to be at.
-          We're constantly growing. We're constantly making mistakes. We're
-          constantly trying to express ourselves and actualize our dreams.`,
-        },
+       
       ];
     return(
-        <main className="bg-rich-black text-off-white h-full">
+        <main className="bg-rich-black text-off-white h-full 
+        flex flex-col items-center justify-center gap-8">
             Featured Rewards
-            <Card>
+            <Card className='text-xl text-off-white p-10 bg-navy-blue
+                transition-shadow duration-300 w-4/5 
+                hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green'>
                 <FeaturedRew />
             </Card>
-            <Tabs id="custom-animation" value="html">
-      <TabsHeader>
-        {data.map(({ label, value }) => (
+            <Tabs id="custom-animation" value="html" className='h-3/5 w-4/5'>
+      <TabsHeader className='bg-transparent h-14' indicatorProps={{
+                        className: 'bg-off-white/20'
+                    }}  >
+        {data.map(({ label, value , icon }, index) => (
           <Tab key={value} value={value}>
-            {label}
+            <div className={`flex items-center gap-2 
+                            font-main font-semibold 
+                            ${index === 0 ? 'text-emerald' :
+                            index === 1 ? 'text-yellow-400' :
+                            index === 2 ? 'text-red-600' : 'text-off-white'}`}
+                        >
+                            {icon}
+                            {label}
+                        </div>
           </Tab>
         ))}
       </TabsHeader>
-      <TabsBody
-        animate={{
-          initial: { y: 250 },
-          mount: { y: 0 },
-          unmount: { y: 250 },
-        }}
+      <TabsBody className='h-full border-b border-emerald/30 rounded-md' 
+                    animate={{
+                        initial: { y: 250 },
+                        mount: { y: 0 },
+                        unmount: { y: 250 },
+                }}
       >
         {data.map(({ value, desc }) => (
           <TabPanel key={value} value={value}>
