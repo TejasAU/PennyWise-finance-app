ae770136cead2352263b3c93912018d7a6054f42
Vishant Kalwani <vishanto95@gmail.com>
2023-10-02 14:33:49 +0530

    Added basic challenges section

--- src/components/challenges/AllChallenges.jsx
+++ src/components/challenges/AllChallenges.jsx
@@ -1,7 +1,87 @@
+import React from 'react'
+import { GiRank1 as EasyIcon, GiRank2 as MediumIcon, GiRank3 as HardIcon } from 'react-icons/gi' 
+import {
+    Tabs,
+    TabsHeader,
+    TabsBody,
+    Tab,
+    TabPanel,
+    TabsHeaderProps
+} from "@material-tailwind/react";
+import { Card } from 'flowbite-react';
+
 export function AllChallenges() {
+    const data = [
+        {
+          label: "Easy",
+          value: "easy",
+          icon: EasyIcon,
+          desc: `Easy challenges are listed here. They are designed to be completed by
+          minimal interaction or first-time users`,
+        },
+        {
+          label: "Medium",
+          value: "medium",
+          icon: MediumIcon,
+          desc: `Medium challenges are listed here. They are designed to be completed by
+          continuous interaction or normal users`,
+        },
+        {
+          label: "Hard",
+          value: "hard",
+          icon: HardIcon,
+          desc: `Hard challenges are listed here. They are designed to be completed by
+          loyal and long-time users`,
+        },
+      ] 
+    
+    
+    
     return(
-        <main className="bg-rich-black text-off-white h-full">
-            This is All Challenges Page
+        <main className="bg-rich-black text-off-white h-full 
+        flex flex-col items-center justify-center gap-8">
+            <Card className='text-xl text-off-white p-4 bg-navy-blue
+                transition-shadow duration-300 w-4/5
+                hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green'>
+                Challenges are a fun and interactive way to supercharge 
+                your financial journey. Whether you're a money-saving beginner or a seasoned
+                finance pro, our challenges offer something for everyone. 
+                Each challenge is designed to help you develop better money habits, achieve 
+                your financial goals, and earn rewards in the form of coins.
+            </Card>
+            <Tabs value="dashboard" className='h-3/5 w-4/5'>
+                <TabsHeader 
+                    className='bg-transparent h-14'
+                    indicatorProps={{
+                        className: 'bg-off-white/20'
+                    }}  
+                >
+                    {data.map(({ label, value, icon }, index) => (
+                    <Tab key={value} value={value}>
+                        <div className={`flex items-center gap-2 
+                        font-main font-semibold ${index === 0 ? 'text-emerald' :
+                                                  index === 1 ? 'text-yellow-400' :
+                                                  index === 2 ? 'text-red-600' : 'text-off-white'}`}
+                        >
+                            {React.createElement(icon, { className: "w-6 h-6" })}
+                            {label}
+                        </div>
+                    </Tab>
+                    ))}
+                </TabsHeader>
+                <TabsBody className='h-full border-b border-emerald/30 rounded-md' 
+                    animate={{
+                        initial: { y: 250 },
+                        mount: { y: 0 },
+                        unmount: { y: 250 },
+                }}>
+                    {data.map(({ value, desc }) => (
+                    <TabPanel key={value} value={value} className='text-off-white text-lg font-main font-normal'>
+                        {desc}
+                    </TabPanel>
+                    ))}
+                </TabsBody>
+            </Tabs>
         </main>
     )
 }
