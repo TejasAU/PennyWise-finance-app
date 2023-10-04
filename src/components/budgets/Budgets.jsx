4a93e889c1fb01ca4684d1d849f4083aceed5c6a
Vishant Kalwani <vishanto95@gmail.com>
2023-10-04 14:52:13 +0530

    Refactored uneccessary components

--- src/components/budgets/Budgets.jsx
+++ src/components/budgets/Budgets.jsx
@@ -1,10 +1,12 @@
 import { Card } from "@material-tailwind/react"
-import { TotalBudgeted } from "./TotalBudgeted"
-import React from "react"
-import { TotalSpent } from "./TotalSpent"
-import { TotalRemaining } from "./TotalRemaning"
+import { Metric } from "@tremor/react"
+import { useState } from "react"
 
 export function Budgets() {
+    const [budgetData, setBudgetData] = useState([
+        {},{},{}
+    ])
+    
     return(
         <main className="bg-rich-black text-off-white h-full">
             <div className="grid grid-cols-6 grid-rows-[repeat(5,7rem)] gap-10 m-20">
@@ -13,7 +15,10 @@ export function Budgets() {
                     hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green
                     flex flex-col gap-2 items-center justify-center"
                 >
-                    <TotalBudgeted />  
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        <Metric className="font-main">&#8377; 2,349</Metric>
+                        <div>Total Budgeted</div>
+                    </div>  
                 </Card>
                 <Card 
                     className="col-span-2 row-span-2 col-start-3 text-xl p-4 
@@ -21,7 +26,10 @@ export function Budgets() {
                     hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green
                     flex flex-col gap-2 items-center justify-center"
                 >
-                    <TotalSpent />
+                     <div className="flex flex-col gap-2 items-center justify-center">
+                        <Metric className="font-main">&#8377; 1,548</Metric>
+                        <div>Total Spent</div>
+                    </div>
                 </Card>
                 <Card 
                     className="col-span-2 row-span-2 col-start-5 text-xl p-4 
@@ -29,21 +37,24 @@ export function Budgets() {
                     hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green
                     flex flex-col gap-2 items-center justify-center"
                 >
-                    <TotalRemaining />
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        <Metric className="font-main">&#8377; 801</Metric>
+                        <div>Total Remaining</div>
+                    </div>
                 </Card>
                 <Card 
                     className="col-span-3 row-span-3 row-start-3 text-xl p-4 
                     text-off-white bg-navy-blue
                     hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green"
                 >
-                    Add a new Budget
+                    <AddBudget />
                 </Card>
                 <Card 
                     className="col-span-3 row-span-3 col-start-4 row-start-3 text-xl p-4 
                     text-off-white bg-navy-blue
                     hover:shadow-[4px_3px_2px_1px] hover:shadow-dark-green"
                 >
-                    Your Budgets
+                    <UserBudgets />
                 </Card>
             </div>
         </main>
