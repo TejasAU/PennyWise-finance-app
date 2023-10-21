1e872e84716e7830a58b0e119671048fcb2f6e7c
yashjugade <yjugade@gmail.com>
2023-10-22 01:03:25 +0530

    Merge index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,8 @@ var Express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var cors = require('cors');
 const multer = require('multer');
-<<<<<<< HEAD
-const new_user=require('./user.api')
-=======
+
 const userCollection = require('./user.api');
->>>>>>> af9479f761f9fb5a028239308f10f682379677f8
 var app = Express();
 app.use(cors());
 
@@ -38,30 +35,7 @@ app.use(cors());
 //     })
 // })
 
-<<<<<<< HEAD
-const {default : mongoose} = require('mongoose');
-mongoose.connect("mongodb+srv://bhaaveshw:Pennywise2125@pennywise.vvlnwo3.mongodb.net/").then(() => {
-    console.log("Mongodb connected to port 3000")
-})
 
-const new_user=new userCollection({
-    _id: 1,
-    name : 'Yash Jugade',
-    email : 'yjugade@gmail.com',
-    password : '12345',
-    contact_no : '9527332805',
-    location :  'Pune'
-    
-})
-
-new_user.save((error) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      console.log('Person saved successfully.');
-    }
-  });
-=======
 const new_user = new userCollection({
     _id : 1,
     name : 'John Doe',
@@ -92,4 +66,4 @@ app.listen(3000, async () => {
     collection.insertOne(new_user);
     // .new_user.save();
 })
->>>>>>> af9479f761f9fb5a028239308f10f682379677f8
+
