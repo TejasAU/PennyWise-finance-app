19471c24eaa285214ca91a446798058981669c35
Bhaavesh Waykole <bhaaveshw@gmail.com>
2023-10-21 21:24:33 +0530

    Connection-via-mongoose

---
+++ api/components/login_signup_api/login.api.js
@@ -0,0 +1,17 @@
+// const {default : mongoose} = require('mongoose');
+// mongoose.connect("mongodb+srv://bhaaveshw:Pennywise2125@pennywise.vvlnwo3.mongodb.net/").then(() => {
+//     console.log("Mongodb connected to port 3000")
+// })
+
+// const Login = new mongoose.Schema({
+//     email : {
+//         type: "String",
+//         required: true,
+//     },
+//     password : {
+//         type: "String",
+//         required: true,
+//     }
+// })
+
+// const loginCollection = mongoose.model("LoginCollection", Login)
--- api/index.js
+++ api/index.js
@@ -6,30 +6,34 @@ const multer = require('multer');
 var app = Express();
 app.use(cors());
 
-var CONNECTION_STRING = "mongodb+srv://bhaaveshw:Pennywise2125@pennywise.vvlnwo3.mongodb.net/?retryWrites=true&w=majority"; 
+// var CONNECTION_STRING = "mongodb+srv://bhaaveshw:Pennywise2125@pennywise.vvlnwo3.mongodb.net/?retryWrites=true&w=majority"; 
 
-var DATABASENAME = "PennyWiseDB";
-var database;
+// var DATABASENAME = "PennyWiseDB";
+// var database;
 
-const client = new MongoClient(CONNECTION_STRING);
-app.listen(3000, async () => {
-    // MongoClient.connect(CONNECTION_STRING,(error, client) => {
-        try{
-            await client.connect();
-            database=client.db(DATABASENAME);
-            console.log("Connected to Database");
-        }catch(error){
-            console.error('Error connecting to MongoDB:', error);
-        }   
-    // })
-})
+// const client = new MongoClient(CONNECTION_STRING);
+// app.listen(3000, async () => {
+//     // MongoClient.connect(CONNECTION_STRING,(error, client) => {
+//         try{
+//             await client.connect();
+//             database=client.db(DATABASENAME);
+//             console.log("Connected to Database");
+//         }catch(error){
+//             console.error('Error connecting to MongoDB:', error);
+//         }   
+//     // })
+// })
 
 
-// Routes :- 
+// // Routes :- 
 
-app.get("/", (req, res) => {
-    // res.send("Welcome");
-    database.collection("PennyWiseDB").find({}).toArray((error,result) => {
-        res.send(result);
-    })
+// app.get("/", (req, res) => {
+//     // res.send("Welcome");
+//     database.collection("PennyWiseDB").find({}).toArray((error,result) => {
+//         res.send(result);
+//     })
+// })
+const {default : mongoose} = require('mongoose');
+mongoose.connect("mongodb+srv://bhaaveshw:Pennywise2125@pennywise.vvlnwo3.mongodb.net/").then(() => {
+    console.log("Mongodb connected to port 3000")
 })
