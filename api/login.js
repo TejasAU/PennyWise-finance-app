6dceebda44df7a8184490c73e94069b8cde6ded2
Bhaavesh Waykole <bhaaveshw@gmail.com>
2023-10-22 20:39:59 +0530

    add-in-db

--- api/login.js
+++ api/login.js
@@ -1,13 +1,18 @@
 import express from "express";
 import User from "./DB_models/User.js"
+import { MongoClient } from "mongodb";
 
 const router = express.Router();
 
+const client = new MongoClient("mongodb+srv://bhaaveshw:Pennywise2125@pennywise.vvlnwo3.mongodb.net/")
+const db = client.db('PennyWiseDB');
+
 router.post('/', async(req, res) => {
     const newUser = new User(req.body)
-
     try{
-        const savedUser = await newUser.save()
+        // const savedUser = await newUser.save(err, User)
+        const User_c = db.collection('User');
+        User_c.insertOne(newUser);
         res.status(200).json(savedUser)
     }catch(err){
         res.status(500).json(err)
